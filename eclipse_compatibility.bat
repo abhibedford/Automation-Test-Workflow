6b1b7d031bf8d3e3a7e3c28da048d882f1d647bb
Abhishek Paul Bedford <30682446+abhibedford@users.noreply.github.com>
2017-08-16 09:58:31 +0530

    Update eclipse_compatibility.bat
    Added comments.

--- eclipse_compatibility.bat
+++ eclipse_compatibility.bat
@@ -1,1 +1,8 @@
+@rem ##########################################################################
+@rem
+@rem  Eclipse Compatibility Command
+@rem  Eclipse plugin in the build file is mandatory
+@rem
+@rem ##########################################################################
+
 gradlew eclipse
